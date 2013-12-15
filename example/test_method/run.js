024b6fb1d823bd08c0cf6364add98e8caebda71f
534591395@qq.com <534591395@qq.com>
2013-12-15 12:15:12 +0800

    init

---
+++ example/test_method/run.js
@@ -0,0 +1,22 @@
+var fk = require("../../"),
+    App = fk.App,
+    app = new App(),
+    static_middle = fk.static; 
+
+
+    app.use(static_middle(__dirname+"/public"));
+
+	/*app.get(function(req,res){
+	   res.write("I am GET method result!");
+	   res.end();
+	});
+
+	app.post(function(req,res){
+		res.write("I am POST method result!");
+		res.end();
+	});
+ 
+*/
+app.listen(3000,function(){
+  console.log("listen 3000 start ...");
+});
