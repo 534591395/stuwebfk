6580d26e5aac57b54dee9a35dc63ebab7310f582
534591395@qq.com <534591395@qq.com>
2013-12-17 20:42:10 +0800

    add route module

--- example/route/run.js
+++ example/route/run.js
@@ -1,20 +1,25 @@
 var fk = require("../../"),
     App = fk.App,
+    query = fk.query,
     app = new App(),
     static_middle = fk.static;
 
-    app.use(static_middle(__dirname+"/public"));
+    //app.use(static_middle(__dirname+"/public"));
 
-	app.get("/about",function(req,res){
-	   res.write("my name is leo!");
+    //app.use(query);
+
+	app.get("/about/:name/:age",function(req,res){
+	   res.write("my name is"+req.params.name);
+        res.write("my age is"+req.params.age);
 	   res.end();
 	});
 
-
+   /*
     app.post("/contact",function(req,res){
 		res.write("contact me us QQ 534591395!");
 		res.end();
 	});
+	*/
  
 
 app.listen(3000,function(){
--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 exports.App = require("./lib/App");
 exports.static = require("./lib/static");
+exports.query = require("./lib/query");
