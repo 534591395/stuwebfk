a06e9dbf5b39001db0185fa2b8be2987a451f6e3
534591395@qq.com <534591395@qq.com>
2014-01-02 17:05:04 +0800

    test

--- example/route/run.js
+++ example/route/run.js
@@ -1,25 +1,25 @@
 var fk = require("../../"),
     App = fk.App,
     query = fk.query,
-    app = new App(),
+    app = new App,
     static_middle = fk.static;
 
     //app.use(static_middle(__dirname+"/public"));
 
-    //app.use(query);
+    app.use(query); //:name/:age
 
-	app.get("/about/:name/:age",function(req,res){
-	   res.write("my name is"+req.params.name);
-        res.write("my age is"+req.params.age);
+	app.get("/about/aa",function(req,res){
+	  // res.write("my name is"+req.params.name);
+       res.write("my age is"+req.params.age);
 	   res.end();
 	});
 
-   /*
+
     app.post("/contact",function(req,res){
 		res.write("contact me us QQ 534591395!");
 		res.end();
 	});
-	*/
+
  
 
 app.listen(3000,function(){
